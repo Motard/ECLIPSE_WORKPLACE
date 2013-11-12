e9e6c3ccaa2b10aff04479bc938274a76418a197
Paulo <pmotard@sapo.pt>
2013-11-12 22:51:06 +0000

    Versao 5.1

--- ECLIPSE WORKSPACE/HelloWorld/src/Director.java
+++ ECLIPSE WORKSPACE/HelloWorld/src/Director.java
@@ -1,21 +1,21 @@
 
 public class Director extends Employee{
 
-	public Director(){
+	//public Director(){
 		
 		//super();//isto pode estar ou nao. Por defeito é construido 1 super()
-	}
+	//}
 	
-	public Director (int empNum, String name){
+	//public Director (int empNum, String name){
 		
 		//this();//chama o constructor por defeito
 		
 		//this.setEmpNum(empNum);
 		//this.setName(name); //pode ser usado assim ou chamamos o construtor do pai. 
 		
-		super(empNum, name);
+		//super(empNum, name);
 		
-	}
+	//}
 	
 	@Override
 	public String toString(){
--- ECLIPSE WORKSPACE/HelloWorld/src/HelloWorld.java
+++ ECLIPSE WORKSPACE/HelloWorld/src/HelloWorld.java
@@ -30,17 +30,20 @@
 			//System.out.println("Hello " + emp.getName() + " from - " + emp.getCity());
 			//System.out.println(emp.toString());
 			
-			Director dir = new Director();
+			//Director dir = new Director();
 			
-			System.out.println(dir.getCity());
+			//System.out.println(dir.getCity());
 			
-			Employee[] employeesList = new Employee[3];
+			//Employee[] employeesList = new Employee[3];
 			
-			employeesList[0] = new Employee(231654,"Paulo Martins");
-			employeesList[1] = new Director(2316,"Joao Silva");
-			employeesList[2] = new Employee(231,"Pedro Correia");
-			//System.out.println(employeesList[1]);
-			((Director)employeesList[0]).firesEveryone();
+//			employeesList[0] = new Employee(231654,"Paulo Martins");
+//			employeesList[1] = new Director(2316,"Joao Silva");
+//			employeesList[2] = new Employee(231,"Pedro Correia");
+//			//System.out.println(employeesList[1]);
+//			((Director)employeesList[0]).firesEveryone();
+			
+			//Director dir1 = new Director();
+			//System.out.println(dir1.getCity());
 			
 		}
 	}
